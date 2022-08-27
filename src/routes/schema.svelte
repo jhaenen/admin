a0e33e54bd8b39467169a0ddccabbeecd2cd8e6b
jhaenen <jasper@haenenweb.nl>
2022-08-27 13:59:19 +0200

    Improve schema UI

--- src/routes/schema.svelte
+++ src/routes/schema.svelte
@@ -9,7 +9,12 @@
     import Result from "@/components/result.svelte";
     import Loader from "@/components/load-ball.svelte";
 
-    let games: Array<Game> = [];
+    interface TimeSlot {
+        time: string;
+        games: Array<Game>;
+    }
+
+    let gamesSorted: Array<TimeSlot> = [];
 
     let error = false;
 
@@ -18,8 +23,8 @@
 
         // Get games from server
         try {
-            const response = await fetch(server + "games");
-            games = await response.json();
+            const response = await fetch(server + "games/sorted");
+            gamesSorted = await response.json();
             error = false;
         } catch (err) {
             error = true;
@@ -37,10 +42,18 @@
         <h1 class="my-2 text-[12vw] leading-none font-thin mi:text-5xl">Dagschema</h1>
 
         <!-- Game list -->
-        {#each games as game (game.id)}
-        <Result gameID={game.id} team1={game.team1} team2={game.team2} time={game.time.substring(0, 5)} poule={game.poule.name} court_num={game.court_num} ref={game.ref} banner color={game.poule.color} on:reload={loadGames}/>
-        {:else}
-            <Loader {error}/>
-        {/each}
+        <div class="flex flex-col items-center w-fit">
+            {#each gamesSorted as slot (slot.time)}
+                <div class="border-b-2 border-slate-300 p-5 flex flex-col items-center justify-center gap-5 lg:flex-row lg:p-10 w-full flex-wrap">  
+                    {#each slot.games as game (game.id)}
+                        <div id={"game" + game.id} class="scroll-mt-5">
+                            <Result gameID={game.id} team1={game.team1} team2={game.team2} time={game.time.substring(0, 5)} poule={game.poule.name} court_num={game.court_num} ref={game.ref} banner color={game.poule.color} on:reload={loadGames}/>
+                        </div>
+                    {/each}
+                </div> 
+            {:else}
+                <Loader {error}/>
+            {/each}
+        </div>
     </div>
 </main>
